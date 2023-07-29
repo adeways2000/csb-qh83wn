fbbac5cf37a2fef6ecc3dcb3678dba2056fdeb38
Code Sandbox <hello@codesandbox.io>
2023-07-29 19:32:09 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.d99e06163b276fe5c26c183b2040c5f0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0a2439e22f91afe7d27ecd5ae4a8e32",
+    "url": "https://adeways2000.github.io/csb-qh83wn/index.html"
+  },
+  {
+    "revision": "e13219111cf99d2d77c2",
+    "url": "https://adeways2000.github.io/csb-qh83wn/static/js/2.53275cb0.chunk.js"
+  },
+  {
+    "revision": "5c1102b94ae59d6f361f",
+    "url": "https://adeways2000.github.io/csb-qh83wn/static/js/main.b4bc1fa2.chunk.js"
+  },
+  {
+    "revision": "956dca173559f443817c",
+    "url": "https://adeways2000.github.io/csb-qh83wn/static/js/runtime-main.2d434001.js"
+  }
+]);
